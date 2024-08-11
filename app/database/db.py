a93e6e5d546c628a1c82e324abf545f8534394e8
VoblaSuperFish <kuznec112233@mail.ru>
2024-08-11 21:52:50 +0500

    Before orm change

--- app/database/db.py
+++ app/database/db.py
@@ -1,14 +1,17 @@
-from .models import Base, engine
 from sqlalchemy.orm import Session
+
+from .models import Base, engine
+
 
 
 def create_table():
+    """Функция создания всех таблиц по мета классу"""
     Base.metadata.create_all(engine)
 
 def drop_table():
+    """Функция удаления всех таблиц по мета классу"""
     Base.metadata.drop_all(engine)
 
 def get_session():
-    #with Session(engine) as session:
-    #    yield session
+    """Получение сессии для базы данных"""
     return Session(engine)
--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import ForeignKey, String, Integer, Boolean, create_engine, ForeignKey
+from sqlalchemy import ForeignKey, create_engine, ForeignKey
+
 from config import BaseSettingsDataBase
 
 db_setting = BaseSettingsDataBase()
@@ -8,12 +9,13 @@ URL_DATABASE_SQLITE = "sqlite:///" + db_setting.DB_URL
 
 engine = create_engine(URL_DATABASE_SQLITE, echo=db_setting.ECHO_QUERY_SQL)
 
-
 class Base(DeclarativeBase):
+    """Мета класс для всех других моделей базы данных"""
     pass
 
 
 class Users(Base):
+    """Модель таблицы пользователей"""
     __tablename__ = 'user'
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
@@ -30,12 +32,18 @@ class Users(Base):
     in_process_mailing: Mapped[bool] = mapped_column(default=False, nullable=False)
 
     def __repr__(self) -> str:
+        """Опеределение отображения объектов модели"""
         return f"Users(vk_id: {self.vk_id})"
     
 class Notes(Base):
+    """Модель таблицы заметок"""
     __tablename__ = 'note'
 
     id:  Mapped[int] = mapped_column(primary_key=True, nullable=False)
     text_note: Mapped[str] = mapped_column()
 
     f_user_id: Mapped[int] = mapped_column(ForeignKey('user.vk_id', ondelete='CASCADE'), nullable=False)
+
+    def __repr__(self) -> str:
+        """Опеределения отображения объектов модели"""
+        return f"Notes(id: {self.id}, f_user_id: {self.f_user_id})"
