45e7c9d651391825cf06b33f64f467eadda0c16a
VoblaSuperFish <kuznec112233@mail.ru>
2024-08-12 21:58:35 +0500

    Comment in function loggs added

--- app/logs/baselog.py
+++ app/logs/baselog.py
@@ -5,20 +5,35 @@ class BaseLoggings:
         file:str, rotation:str, level:str,          
         format:str="{time} {level} {message}", 
         seriallize:bool=False):
-
+        """
+        Инициализация базовых настроек для логирования
+        >file - файл, куда будут помещены все логи.
+        >rotation - параметр, при котором определяется причина обновления
+        файла логов (может быть вес файла или время).
+        >level - начиная с какого уровня отлавливать логи.
+        >format - формат записи логов в файл.
+        >seriallize - сериализация данных, если будет True, логи
+        будут записываться в формате json, но и расширение нужно json.
+        >>>см. https://loguru.readthedocs.io/en/stable/overview.html
+        """
         logger.add(file, format=format, level=level, rotation=rotation, 
                 compression='zip',serialize=seriallize)
     
-    #Уровень лога расположен от самого лёгкого до самого критичного
     def debug(self, message):
+        """Меньший уровень лога, в основном для отлаживания программы на уровне
+        проектирования"""
         logger.debug(message)
     def info(self, message):
+        """Подходит для пометки основной информации об приложении"""
         logger.info(message)
     def warning(self, message):
+        """Лог, на который стоит обратить внимание"""
         logger.warning(message)
     def error(self, message):
-        logger.critical(message)
+        """Сообщение об возникшей ошибке приложения"""
+        logger.error(message)
     def critical(self, message):
+        """Критическая ошибка, которая влияет на жизнь программы"""
         logger.critical(message)
 
 #Настройка, куда будут идти все логи
